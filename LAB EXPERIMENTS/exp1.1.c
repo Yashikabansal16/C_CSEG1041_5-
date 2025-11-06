e6104b09701d4c057e518eea11fac9971a10bf10
Yashika Bansal <yashikabansal160506@gmail.com>
2025-11-06 22:26:07 +0530

    Add files via upload

---
+++ LAB EXPERIMENTS/exp1.1.c
@@ -0,0 +1,9 @@
+/* Experiment 1: Installation, Environment Setup and starting with C language
+
+1. Write a C program to print “ Hello World” */
+
+#include<stdio.h>
+int main() {
+    printf("HELLO WORLD!");
+    return 0;
+}
