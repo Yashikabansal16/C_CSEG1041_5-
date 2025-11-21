ca8aa3103b7939f9c822af75d7323bc7a7e07136
Yashika Bansal <yashikabansal160506@gmail.com>
2025-11-21 14:35:14 +0530

    Delete exp1.2.c

--- LAB EXPERIMENTS/exp1.2.c
+++
@@ -1,7 +0,0 @@
-/*2. Write a C Program to print the address in multiple lines (new line). */
-
-#include<stdio.h>
-int main() {
-    printf("N-1/20 \nBUDH VIHAR PHASE-1 \nDELHI-86");
-    return 0;
-}
