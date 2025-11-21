108b6307f989649d74a03d6f26e27dcd794367ee
Yashika Bansal <yashikabansal160506@gmail.com>
2025-11-21 14:34:57 +0530

    Delete exp1.1.c

--- LAB EXPERIMENTS/exp1.1.c
+++
@@ -1,9 +0,0 @@
-/* Experiment 1: Installation, Environment Setup and starting with C language
-
-1. Write a C program to print “ Hello World” */
-
-#include<stdio.h>
-int main() {
-    printf("HELLO WORLD!");
-    return 0;
-}
