b7cf285f095c97c1a19f37d135f1eca5bb9bfb16
Yashika Bansal <yashikabansal160506@gmail.com>
2025-11-21 14:36:46 +0530

    Add files via upload

---
+++ LAB EXPERIMENTS/exp1.3.c
@@ -0,0 +1,15 @@
+/*3. Write a program that prompts the user to enter their name and age.*/
+
+#include<stdio.h>
+int main() {
+    char name[50];
+    int age;
+    
+    printf("Enter your name: ");
+    scanf("%s",&name);
+
+    printf("Enter your age: ");
+    scanf("%d",&age);
+
+    return 0;
+}
